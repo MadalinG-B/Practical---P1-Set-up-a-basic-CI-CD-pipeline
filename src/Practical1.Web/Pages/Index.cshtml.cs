0e0606a548f6434391c67179989d8fc2ff75b39c
Madalin Gabriel Boia <boiamadalingabriel@proton.me>
2026-05-16 14:17:58 +0200

    Initial commit: Basic app with two tests.

---
+++ src/Practical1.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practical1.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ tests/Practical1.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿namespace Practical1.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void AlwaysTrue_ShouldPass()
+    {
+      var expected = true;
+      
+      var actual = true;
+
+      Assert.Equal(expected,actual);
+    }
+
+    [Fact]
+    public void StringNotEmpty_ShouldPass()
+    {
+      var value = "Hello, Azure DevOps!";
+
+      Assert.False(string.IsNullOrEmpty(value));
+    }
+
+}
